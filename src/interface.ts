1028029a3b98c80c9bef20a72130900cb54a5b3f
Omkrishna-git <omdpatil07@gmail.com>
2025-04-05 15:25:46 +0530

    interface completed

---
+++ src/interface.ts
@@ -0,0 +1,55 @@
+interface Person {
+    name: string;
+    age: number;
+    greet(): void;
+}
+
+interface Users {
+	firstName: string;
+	lastName: string;
+	email: string;
+	age: number;
+}
+
+const person: Person = {
+    name: "John Doe",
+    age: 30,
+    greet() {
+        console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+    },
+};
+
+function isEven(num : number) : boolean {
+    if(!(num&1)) return true;
+    else return false;
+}
+
+interface preson {
+    name : string;
+    age : number;
+    address:{
+        city: string;
+        state: string;
+        country: string;
+        pincode : number;
+      };
+    entollmentNo : number;
+    isCitizen : boolean;
+}
+
+function isLegal(p : preson) : boolean {
+    if(p.isCitizen && p.age >= 18) return true;
+    else return false;
+}
+
+person.greet();
+
+interface people {
+    name :string;
+    age:number;
+    greete: () => string;
+}
+
+let greete = (p : people) : string => {
+    return `Hello ${p.name}, you are ${p.age} years old.`;
+}
---
+++ src/types.ts
@@ -0,0 +1,14 @@
+type User = {
+	firstName: string;
+	lastName: string;
+	age: number
+}
+
+type StringOrNumber = string | number;
+
+function printId(id: StringOrNumber) {
+  console.log(`ID: ${id}`);
+}
+
+printId(101); // ID: 101
+printId("202"); // ID: 202
